7351dc9bc13fff433e6dfb6a910480a39c0ab460
Lexag <lexa.grebnets@gmail.com>
2025-07-04 15:55:44 +0200

    tempo changes, repeats and rehearsal marks in parser and host

--- src/metronome.rs
+++ src/metronome.rs
@@ -103,13 +103,14 @@ impl audio::source::AudioSource for Metronome {
     ) -> Result<&[f32], jack::Error> {
         self.status = status.clone();
         if status.running {
+            let beat = self.cue.get_beat(self.beat_idx).unwrap();
             let res = self.cue.get_beat(self.next_beat_idx);
             if let Err(err) = res {
                 return Ok(&[0f32; 2048][0..ps.n_frames() as usize]);
             }
             let next_beat = res.unwrap();
             let t_us = c.frames_to_time(c.frame_time());
-            let next_schd_t_us: u64 = self.last_beat_time + (next_beat.length * 1000) as u64;
+            let next_schd_t_us: u64 = self.last_beat_time + (beat.length * 1000) as u64;
 
             //println!(
             //    "t: {}, sch: {}, lb: {}",
@@ -125,7 +126,6 @@ impl audio::source::AudioSource for Metronome {
                 } else {
                     self.last_beat_time = next_schd_t_us;
                 }
-                let beat = self.cue.get_beat(self.beat_idx).unwrap();
                 for event in beat.events {
                     self.handle_event(event);
                 }
